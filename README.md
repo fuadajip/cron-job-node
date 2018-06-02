ac16bf18c5eeb52d073a9895ff012a1931658205
fuadajip <fuadajipratomo@gmail.com>
2018-06-02 16:07:34 +0700

    update readme add example cron job delete error log

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cron-job-node
 Example implementation of cron job with nodejs
+
+## Prerequisites
+- Make sure you instal node v8+
+- Install yarn
+
+## Installation
+Install depedencies form package.json
+```bash
+    yarn install
+```
+
+## Run
+Uncomment each tash to try each cron job individually
+
+```bash
+node index.js
+```
+
+### Cron short description
+```
+* * * * * *
+| | | | | |
+| | | | | day of week
+| | | | month
+| | | day of month
+| | hour
+| minute
+second ( optional )
+```
--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const fs = require("fs");
 const app = express();
 
 // schedule task to be run on the server
-cron.schedule("* * * * *", ()=>{
-    console.log("running a task every minute")
+cron.schedule("* * * * *", () => {
+  console.log("running a task every minute");
+});
+
+// cron job example to delete error log every 15st of month
+cron.schedule("* * 15 * *", function() {
+  console.log("---------------------");
+  console.log("Running Cron Job");
+  fs.unlink("./error.log", err => {
+    if (err) throw err;
+    console.log("Error file succesfully deleted");
+  });
 });
 
 app.listen(3218);
